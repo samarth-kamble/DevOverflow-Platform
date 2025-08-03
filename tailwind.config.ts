fa8761dc6efbf5d0afeea707be9d7525f68134ec
Samarth Kamble <kamblesamarth04@gmail.com>
2025-08-03 21:01:11 +0530

    feat: implement authentication layout and forms, update Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
   darkMode: ["class"],
@@ -46,12 +47,18 @@ const config: Config = {
         "space-grotesk": ["var(--font-space-grotesk)"],
       },
       borderRadius: {
+        "2": "8px",
+        "1.5": "6px",
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      backgroundImage: {
+        "auth-dark": 'url("/images/auth-dark.png")',
+        "auth-light": 'url("/images/auth-light.png")',
+      },
     },
+    plugins: [tailwindcssAnimate],
   },
-  plugins: [require("tailwindcss-animate")],
 };
 export default config;
