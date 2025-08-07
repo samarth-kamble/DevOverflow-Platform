0c81f46b7f5fbed39b386f1f36b5952f741dc479
Samarth Kamble <kamblesamarth04@gmail.com>
2025-08-07 15:57:44 +0530

    implement the increase view second approach

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -15,8 +15,6 @@ import {
   IncrementViewsSchema,
   PaginatedSearchParamsSchema,
 } from "../validations";
-import { revalidatePath } from "next/cache";
-import ROUTES from "@/constants/routes";
 
 export async function createQuestion(
   params: CreateQuestionParams
@@ -317,8 +315,6 @@ export async function incrementViews(
     question.views += 1;
 
     await question.save();
-
-    revalidatePath(ROUTES.QUESTION(questionId));
 
     return {
       success: true,
