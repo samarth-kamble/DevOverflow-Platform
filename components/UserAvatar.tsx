14404fcf34e85df3415b5e7e1aed375ec2e0a74e
Samarth Kamble <kamblesamarth04@gmail.com>
2025-08-09 16:43:50 +0530

    Implement get all user action and display all users:

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -31,21 +31,20 @@ const UserAvatar = ({
 
   return (
     <Link href={ROUTES.PROFILE(id)}>
-      <Avatar className={className}>
+      <Avatar className={cn("relative", className)}>
         {imageUrl ? (
           <Image
             src={imageUrl}
             alt={name}
             className="object-cover"
-            width={36}
-            height={36}
+            fill
             quality={100}
           />
         ) : (
           <AvatarFallback
             className={cn(
               "primary-gradient font-space-grotesk font-bold tracking-wider text-white",
-              fallbackClassName
+              fallbackClassName,
             )}
           >
             {initials}
