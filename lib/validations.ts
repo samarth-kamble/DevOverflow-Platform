2c9991dac3528295b2fec7a7540d206084a4ea03
Samarth Kamble <kamblesamarth04@gmail.com>
2025-08-10 23:27:58 +0530

    Implement edit and delete button with action

--- lib/validations.ts
+++ lib/validations.ts
@@ -208,3 +208,11 @@ export const GetUsersAnswersSchema = PaginatedSearchParamsSchema.extend({
 export const GetUsersTagsSchema = z.object({
   userId: z.string().min(1, { message: "User ID is required." }),
 });
+
+export const DeleteQuestionSchema = z.object({
+  questionId: z.string().min(1, "Question ID is required"),
+});
+
+export const DeleteAnswerSchema = z.object({
+  answerId: z.string().min(1, "Answer ID is required"),
+});
