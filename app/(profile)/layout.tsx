a72f77e41e46aa70397b9d0228a123f07695a741
Samarth Kamble <kamblesamarth04@gmail.com>
2025-08-10 15:42:51 +0530

    Implement profile page layout and user data retrieval

---
+++ app/(profile)/layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+
+import LeftSidebar from "@/components/navigation/LeftSidebar";
+import Navbar from "@/components/navigation/navbar";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="background-light850_dark100 relative">
+      <Navbar />
+
+      <div className="flex">
+        <LeftSidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default RootLayout;
