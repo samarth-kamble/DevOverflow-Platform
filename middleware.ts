451efa3dc5479eaf71dd392a006e873e26c09d09
Samarth Kamble <kamblesamarth04@gmail.com>
2025-08-11 23:36:34 +0530

    Fix Deployment issue

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,15 @@
 export { auth as middleware } from "@/auth";
+// Force Node.js runtime for the middleware
+export const config = {
+  runtime: "nodejs",
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
